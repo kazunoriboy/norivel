123fe1ef0fba113cb81f2549494ce5633cb89941
GitButler <gitbutler@gitbutler.com>
2024-06-23 10:00:36 +0900

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - main (refs/gitbutler/main)
       branch head: 333213ab005bd68185d3bcfcc8a8bb4a5beaf19b
       - bootstrap/app.php
       - public/index.php
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-phpinfo();
+
+define('NORIVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+require __DIR__.'/../vendor/autoload.php';
